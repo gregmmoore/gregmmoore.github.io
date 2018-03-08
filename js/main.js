4284ea5e0ca57f2bac3b37ea9b925ad33dfae95c
Gregory Moore <GMONEY@Gregorys-MacBook-Pro.local>
2018-03-07 20:39:21 -0600

    Initial Commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDXFRtzoyqQ5mrv_u6vXZXpVyx8E1Y9Lsc",
+    authDomain: "restaraunt-site.firebaseapp.com",
+    databaseURL: "https://restaraunt-site.firebaseio.com",
+    projectId: "restaraunt-site",
+    storageBucket: "restaraunt-site.appspot.com",
+    messagingSenderId: "946381516268"
+  };
+  firebase.initializeApp(config);
+
+  // Connect to Database
+var database = firebase.database();
+
+var reservationData = {};
+
 function initMap() {
 	var map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 40.8054491, lng: -73.9654415},
